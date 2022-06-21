f7a340d46693f8fc752a3d85554ee350c22c9545
Agnes <koinageagness@gmail.com>
2022-06-21 15:39:31 +0300

    configure databases

--- project/settings.py
+++ project/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'weatheryourdestination',
+        'USER': 'nessie',
+        'PASSWORD': 'agnes1234',
+        'PORT': '',
     }
 }
 
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
