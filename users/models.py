83f68b570b3a4d1f74f98a93ec368b8a151e1f11
Agnes <koinageagness@gmail.com>
2022-06-22 21:20:42 +0300

    add auth syles

--- users/models.py
+++ users/models.py
@@ -4,14 +4,14 @@ from pyuploadcare.dj.models import ImageField
 import datetime as dt
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from app.models import NeighbourHood
+from app.models import city
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_photo = ImageField(blank=True, manual_crop="")
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=60, blank=True)
     contact = models.CharField(max_length=60,blank=True)
-    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True,blank=True)
+    neighbourhood = models.ForeignKey(city, on_delete=models.SET_NULL, null=True,blank=True)
     create_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
